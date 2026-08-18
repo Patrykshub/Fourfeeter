2eba8c7a9ae5ece91af5583bf5341e2371f24c17
Patrykshub <blacha360@gmail.com>
2026-08-18 14:39:24 +0200

    Use common.addNew translation key in EmptyState's add button
    Was hardcoded Polish text ("Dodaj nowy"), unlike every other admin
    add button which already goes through react-intl.

--- src/components/common/EmptyState.tsx
+++ src/components/common/EmptyState.tsx
@@ -1,18 +1,24 @@
+import { useIntl } from "react-intl";
+
 interface IEmptyStateProps {
   message: string;
   isAdmin: boolean;
   onAdd: () => void;
 }
 
-export const EmptyState = ({ message, isAdmin, onAdd }: IEmptyStateProps) => (
-  <div className="text-center text-gray-400 py-16">
-    {message}
-    {isAdmin && (
-      <div className="mt-4">
-        <button onClick={onAdd} className="btn-admin">
-          Dodaj nowy
-        </button>
-      </div>
-    )}
-  </div>
-);
+export const EmptyState = ({ message, isAdmin, onAdd }: IEmptyStateProps) => {
+  const intl = useIntl();
+
+  return (
+    <div className="text-center text-gray-400 py-16">
+      {message}
+      {isAdmin && (
+        <div className="mt-4">
+          <button onClick={onAdd} className="btn-admin">
+            {intl.formatMessage({ id: "common.addNew" })}
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
