029aa67f8b4f18e0294486f8ff9e643c46907baa
Patrykshub <blacha360@gmail.com>
2026-09-16 12:58:47 +0200

    Stack the all-posts grid into a single column on small screens

--- src/components/home/HomeView.tsx
+++ src/components/home/HomeView.tsx
@@ -91,7 +91,7 @@ export const HomeView = ({
           variants={staggerContainer}
           initial="hidden"
           animate="show"
-          className="grid grid-cols-2 lg:grid-cols-3 gap-6"
+          className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6"
         >
           {posts.map((post) => (
             <HomePostCard
